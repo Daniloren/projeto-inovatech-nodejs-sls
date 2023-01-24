ade3d515e0d41e6614c1f09c4fdc526aa76c147e
Daniella Furquim <daniella.lorenzano@gmail.com>
2023-01-24 20:55:16 -0300

    adicionamos options users

--- handler.js
+++ handler.js
@@ -11,7 +11,8 @@ const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
 const crypto = require("crypto");
 
 app.use(express.json());
-//endpoints users
+
+//Pesquisa de usuários usando ID
 app.get("/users/:userId", async function (req, res) {
   const params = {
     TableName: USERS_TABLE,
@@ -35,6 +36,8 @@ app.get("/users/:userId", async function (req, res) {
     res.status(500).json({ error: "Could not retreive user" });
   }
 });
+
+//Lista todos os usuários cadastrados no banco (DynamoDB)
 app.get("/users", async function (req, res) {
   const params = {
     TableName: USERS_TABLE,
@@ -56,6 +59,7 @@ app.get("/users", async function (req, res) {
   }
 });
 
+//Cadastro de usuários
 app.post("/users", async function (req, res) {
   const {
     userId,
@@ -112,6 +116,9 @@ app.post("/users", async function (req, res) {
     res.status(500).json({ error: "Could not create user" });
   }
 });
+
+//Verifica se existe um usuário com login e senha informados.
+//Senha não está criptografada.
 app.post("/login", async function (req, res) {
   const { email, password } = req.body;
   console.log("request:", email, password);
@@ -123,24 +130,30 @@ app.post("/login", async function (req, res) {
   let scanResults = [];
   let items;
   try {
+    //Busca todos os usuarios no banco de dados
     items = await dynamoDbClient.scan(params).promise();
     items.Items.forEach((item) => {
+      //Se algum usuario tiver o login e senha informados, ele é adicionado em uma lista
       if (item.email === email && item.password === password) {
         scanResults.push(item);
       }
     });
 
+    //Se a lista tiver só um usuario (unico com login e senha), retorna sucesso, com dados do usuário
+    //Senão, retorna "Usuario não encontrado"
     if (scanResults.length != 1) {
       res.status(404).json({ error: "User not found" });
     } else {
-      res.json({ user: items.Items[0] });
+      res.json(items.Items[0]);
     }
   } catch (error) {
+    //Em caso de erro, mostra mensagem de usuário não encontrado
     console.log(error);
     res.status(500).json({ error: "Could not found user" });
   }
 });
-//endpoints ads
+
+//Busca todos os ads (anuncios) cadastrados no banco
 app.get("/ads", async function (req, res) {
   const params = {
     TableName: ADS_TABLE,
@@ -161,6 +174,8 @@ app.get("/ads", async function (req, res) {
     res.status(500).json({ error: "Could not list ads" });
   }
 });
+
+//Adiciona e atualiza Ads (anuncios)
 app.post("/ads", async function (req, res) {
   const { adsId, name, src, altText, caption, align, link } = req.body;
   if (typeof adsId !== "string") {
@@ -191,7 +206,7 @@ app.post("/ads", async function (req, res) {
   }
 });
 
-//endpoints cursos
+//Lista todos os cursos
 app.get("/cursos", async function (req, res) {
   const params = {
     TableName: CURSOS_TABLE,
@@ -201,17 +216,21 @@ app.get("/cursos", async function (req, res) {
   let items;
   try {
     do {
+      //Busca todos os cursos disponiveis no banco de dados
       items = await dynamoDbClient.scan(params).promise();
+      //Adiciona em um Array
       items.Items.forEach((item) => scanResults.push(item));
       params.ExclusiveStartKey = items.LastEvaluatedKey;
     } while (typeof items.LastEvaluatedKey != "undefined");
 
+    //retorna os cursos
     res.json({ cursos: items });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Could not list cursos" });
   }
 });
+//Busca cursos usando o ID
 app.get("/cursos/:cursosId", async function (req, res) {
   const params = {
     TableName: CURSOS_TABLE,
@@ -256,6 +275,8 @@ app.get("/cursos/:cursosId", async function (req, res) {
     res.status(500).json({ error: "Could not retreive curso" });
   }
 });
+
+//Cadastra Cursos
 app.post("/cursos", async function (req, res) {
   const {
     cursosId,
@@ -304,6 +325,29 @@ app.post("/cursos", async function (req, res) {
     console.log(error);
     res.status(500).json({ error: "Could not create cursos" });
   }
+});
+
+//Metodo OPTIONS é usado para fazer chamadas de dominios diferentes. Ex:
+//http://www.inovatech.com.br.s3-website-us-east-1.amazonaws.com/
+//chamando
+//https://2uhw4x03se.execute-api.us-east-1.amazonaws.com
+app.options("/cursos", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.send(200);
+});
+app.options("/login", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.send(200);
+});
+app.options("/users", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.send(200);
 });
 
 app.use((req, res, next) => {
